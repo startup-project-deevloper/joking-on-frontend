df2e86029d90ece9cfc4e4d3c83f26be3040351c
Patrick <51328607+unenunciate@users.noreply.github.com>
2021-10-10 15:10:48 -0400

    removed packages

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-const withOptimizedImages = require("next-optimized-images");
-
-module.exports = withOptimizedImages({
-  webpack: (config) => {
-    return config;
-  }
-}
-);
