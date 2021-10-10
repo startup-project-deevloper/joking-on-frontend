d1b458ee32441ac5e6625a28b021a5537d89c85e
Patrick <51328607+unenunciate@users.noreply.github.com>
2021-10-10 15:05:47 -0400

    removed not needed packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
 const webpack = require("webpack");
-const withGraphQL = require("next-plugin-graphql");
 const withOptimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages(
-  withGraphQL({
-    webpack: (config) => {
-      config.plugins.push(
-        new webpack.ContextReplacementPlugin(
-          /graphql-language-service-interface[\\/]dist$/,
-          new RegExp(`^\\./.*\\.js$`)
-        )
-      );
-      return config;
-    },
-  })
+module.exports = withOptimizedImages({
+  webpack: (config) => {
+    return config;
+  }
+}
 );
